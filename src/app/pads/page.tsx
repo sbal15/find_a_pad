52c76d5700647c2ec0ed5865e4ae42eef114da55
sbal15 <samyuktabalaji@gmail.com>
2024-03-03 01:14:04 -0500

    bgcolor

--- src/app/pads/page.tsx
+++ src/app/pads/page.tsx
@@ -4,7 +4,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Ced() {
   return(
-    <Tabs defaultValue="account" className="w-[400px]">
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
+    <Tabs defaultValue="account" className="w-[400px] w-1 ">
     <TabsList>
       <TabsTrigger value="account">Account</TabsTrigger>
       <TabsTrigger value="password">Password</TabsTrigger>
@@ -12,5 +13,6 @@ export default function Ced() {
     <TabsContent value="account">Make changes to your account here.</TabsContent>
     <TabsContent value="password">Change your password here.</TabsContent>
   </Tabs>  
+  </div>
   )
 }
