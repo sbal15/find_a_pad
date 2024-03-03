6cf64e62b109bdc3cd147e35d632fcbf5f9bdd57
shayan <arunshayan@gmail.com>
2024-03-02 22:29:58 -0800

    weird bug

--- src/app/pads/page.tsx
+++ src/app/pads/page.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 export default function Ced() {
-  return(
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
-    <Tabs defaultValue="account" className="w-[400px] w-1 ">
-    <TabsList>
-      <TabsTrigger value="account">Account</TabsTrigger>
-      <TabsTrigger value="password">Password</TabsTrigger>
-    </TabsList>
-    <TabsContent value="account">Make changes to your account here.</TabsContent>
-    <TabsContent value="password">Change your password here.</TabsContent>
-  </Tabs>  
-  </div>
-  )
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... min-h-screen">
+      <Tabs defaultValue="account" className="w-[400px] w-1">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
